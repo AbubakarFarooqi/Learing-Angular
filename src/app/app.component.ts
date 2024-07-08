4360f1d80391380143e9e2929d3290bd7d8ea72d
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-07-08 15:01:43 +0500

    Learned signals input and output signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HeaderCompoenent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,9 @@ import { UserComponent } from './user/user.component';
 })
 export class AppComponent {
   title = 'first-angular-project';
+  users = DUMMY_USERS;
+
+  onUserSelect(id:string){
+    console.log(id);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input,input,computed,Output,EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { MapType } from '@angular/compiler';
 
@@ -10,13 +10,34 @@ import { MapType } from '@angular/compiler';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-    selectedUser = DUMMY_USERS[Math.floor(Math.random()*DUMMY_USERS.length)];
+    //Zone.js 
+    // selectedUser = DUMMY_USERS[Math.floor(Math.random()*DUMMY_USERS.length)];
+   
+    //Input using Input Decorator
+    @Input({required:true}) id!: string
+    @Input({required:true}) avatar!: string
+    @Input({required:true}) name!: string
+
+    //Output Data
+    @Output() select = new EventEmitter() 
+
+    //Input using input signal
+    // avatar = input.required<string>()
+    // name = input.required<string>()
+
+    // Signal
+    // selectedUser = signal(DUMMY_USERS[Math.floor(Math.random()*DUMMY_USERS.length)]);
+    
+   // Compted value using getter
     get imagePath(){
-      return `assets/users/${this.selectedUser.avatar}`
+      return `assets/users/${this.avatar}`
     }
 
+    //Computed Value using Computed
+   // imagePath = computed(()=>`assets/users/${this.avatar()}`)
+
+
     onSelectedUser() {
-      console.log(`button clicked`)
-      this.selectedUser = DUMMY_USERS[Math.floor(Math.random()*DUMMY_USERS.length)];
+      this.select.emit(this.id)
     }
 }
