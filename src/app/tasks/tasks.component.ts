190b50ec0192effd8d5871ea0d1427b759bb236e
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-07-10 12:08:16 +0500

    Add tasks and user-tasks component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component,Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+import { UserTasksComponent } from './user-tasks/user-tasks.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [UserTasksComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
