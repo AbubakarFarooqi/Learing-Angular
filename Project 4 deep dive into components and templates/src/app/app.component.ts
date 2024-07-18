b9189dfcbf825ff17fb89651cc1869db771e97c2
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-07-18 10:20:40 +0500

    app-compoenent code is separated into different compoenents

--- Project 4 deep dive into components and templates/src/app/app.component.ts
+++ Project 4 deep dive into components and templates/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { ServerStatusComponent } from "./dashbord/server-status/server-status.component";
+import { TrafficComponent } from "./dashbord/traffic/traffic.component";
+import { TicketsComponent } from "./dashbord/tickets/tickets.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
 })
 export class AppComponent {
   dummyTrafficData = [
