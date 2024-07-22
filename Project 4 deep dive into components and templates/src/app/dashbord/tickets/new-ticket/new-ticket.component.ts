22a30cdce474e2d7a95ae90b6f3cb7ad27f94fba
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-07-22 11:32:51 +0500

    reset form after submission using viewChild decorator

--- Project 4 deep dive into components and templates/src/app/dashbord/tickets/new-ticket/new-ticket.component.ts
+++ Project 4 deep dive into components and templates/src/app/dashbord/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -12,9 +12,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
 
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   onSubmitForm(title:string, request:string){
     console.dir(title)
     console.dir(request)
+    this.form?.nativeElement.reset()
   }
 
 }
