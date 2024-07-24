7c3d839b7c5a93131eb0d844e774f510713d52c3
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-07-24 16:52:52 +0500

    App wide error handling

--- Project 7 working with backend/src/app/places/places.service.ts
+++ Project 7 working with backend/src/app/places/places.service.ts
@@ -1,14 +1,16 @@
 import { inject, Injectable, signal } from '@angular/core';
 
 import { Place } from './place.model';
-import { map, pipe, tap } from 'rxjs';
+import { map, pipe, tap, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
   private userPlaces = signal<Place[]>([]);
+  private _errorService = inject(ErrorService);
 
   loadedUserPlaces = this.userPlaces.asReadonly();
   private _httpCLient = inject(HttpClient);
@@ -19,7 +21,15 @@ export class PlacesService {
       .pipe(map((resData) => resData.places));
   }
   loadAvailablePlaces() {
-    return this.fetchPlaces('http://localhost:3000/places');
+    return this.fetchPlaces('http://localhost:3000/places').pipe(
+      tap({
+        error: () => {
+          this._errorService.showError(
+            'Something wenrt wrong while loading avaible places'
+          );
+        },
+      })
+    );
   }
 
   loadUserPlaces() {
@@ -28,14 +38,37 @@ export class PlacesService {
         next: (userPlaces) => {
           this.userPlaces.set(userPlaces);
         },
+        error: () => {
+          this._errorService.showError(
+            'Something wenrt wrong while loading user places'
+          );
+        },
       })
     );
   }
 
   addPlaceToUserPlaces(place: Place) {
-    return this._httpCLient.put('http://localhost:3000/user-places', {
-      placeId: place.id,
-    });
+    const prevPlaces = this.userPlaces();
+    if (!this.userPlaces().some((p) => p.id === place.id)) {
+      this.userPlaces.update((places) => {
+        return [...places, place];
+      });
+    }
+
+    return this._httpCLient
+      .put('http://localhost:3000/user-places', {
+        placeId: place.id,
+      })
+      .pipe(
+        tap({
+          error: () => {
+            this.userPlaces.set(prevPlaces);
+            this._errorService.showError(
+              'Something wenrt wrong while loading user places'
+            );
+          },
+        })
+      );
   }
 
   removeUserPlace(place: Place) {}
