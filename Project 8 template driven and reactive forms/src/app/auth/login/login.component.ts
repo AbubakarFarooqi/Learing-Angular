7194b8dd376234f9209e8830f98fb5818b43dd4d
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-07-25 16:47:39 +0500

    Save email in local storage and load  it to email control on start

--- Project 8 template driven and reactive forms/src/app/auth/login/login.component.ts
+++ Project 8 template driven and reactive forms/src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
+import { EmailValidator, FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +16,31 @@ import { FormsModule, NgForm } from '@angular/forms';
   imports: [FormsModule],
 })
 export class LoginComponent {
+  private form = viewChild<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+  constructor() {
+    afterNextRender(() => {
+      const savedEmail = window.localStorage.getItem('email');
+      if (savedEmail) {
+        setTimeout(() => {
+          this.form()?.controls['email'].setValue(savedEmail);
+        });
+      }
+
+      const subscription = this.form()
+        ?.valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) => {
+            window.localStorage.setItem('email', value.email);
+          },
+        });
+
+      this.destroyRef.onDestroy(() => {
+        subscription?.unsubscribe();
+      });
+    });
+  }
+
   onSubmitForm(formData: NgForm) {
     console.log(formData.form);
   }
