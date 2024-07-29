3be678880893a9bf944f2d9a9537fdffa4c62262
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-07-29 17:17:16 +0500

    add controls in signup form

--- Project 8 template driven and reactive forms/src/app/auth/signup/signup.component.ts
+++ Project 8 template driven and reactive forms/src/app/auth/signup/signup.component.ts
@@ -19,16 +19,28 @@ export class SignupComponent {
     emailControl: new FormControl('', {
       validators: [Validators.required],
     }),
-    passwordControl: new FormControl('', {
-      validators: [Validators.required],
+    passwordsSubForm: new FormGroup({
+      passwordControl: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      confirmPasswordControl: new FormControl('', {
+        validators: [Validators.required],
+      }),
     }),
-    confirmPasswordControl: new FormControl('', {
-      validators: [Validators.required],
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    addressSubform: new FormGroup({
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
     }),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', { validators: [Validators.required] }),
+    agree: new FormControl(true, { validators: [Validators.required] }),
   });
   onSubmit() {
-    console.log(this.signupForm.controls.emailControl.value);
-    console.log(this.signupForm.controls.passwordControl.value);
-    console.log(this.signupForm.controls.confirmPasswordControl.value);
+    console.log(this.signupForm);
   }
 }
