9d0adedf43bde3df5295711431a60eb8ccac2e9f
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-07-29 11:15:41 +0500

    update login conmponent ts and html file.

--- Project 8 template driven and reactive forms/src/app/auth/login/login.component.ts
+++ Project 8 template driven and reactive forms/src/app/auth/login/login.component.ts
@@ -1,47 +1,9 @@
-import {
-  afterNextRender,
-  Component,
-  DestroyRef,
-  inject,
-  viewChild,
-} from '@angular/core';
-import { EmailValidator, FormsModule, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [FormsModule],
 })
-export class LoginComponent {
-  private form = viewChild<NgForm>('form');
-  private destroyRef = inject(DestroyRef);
-  constructor() {
-    afterNextRender(() => {
-      const savedEmail = window.localStorage.getItem('email');
-      if (savedEmail) {
-        setTimeout(() => {
-          this.form()?.controls['email'].setValue(savedEmail);
-        });
-      }
-
-      const subscription = this.form()
-        ?.valueChanges?.pipe(debounceTime(500))
-        .subscribe({
-          next: (value) => {
-            window.localStorage.setItem('email', value.email);
-          },
-        });
-
-      this.destroyRef.onDestroy(() => {
-        subscription?.unsubscribe();
-      });
-    });
-  }
-
-  onSubmitForm(formData: NgForm) {
-    console.log(formData.form);
-  }
-}
+export class LoginComponent {}
