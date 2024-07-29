28fa3728efcecbc0623cc0814d3d0b7182390e27
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-07-29 15:46:38 +0500

    start working on signup form

--- Project 8 template driven and reactive forms/src/app/auth/signup/signup.component.ts
+++ Project 8 template driven and reactive forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { consumerPollProducersForChange } from '@angular/core/primitives/signals';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signupForm = new FormGroup({
+    emailControl: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    passwordControl: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    confirmPasswordControl: new FormControl('', {
+      validators: [Validators.required],
+    }),
+  });
+  onSubmit() {
+    console.log(this.signupForm.controls.emailControl.value);
+    console.log(this.signupForm.controls.passwordControl.value);
+    console.log(this.signupForm.controls.confirmPasswordControl.value);
+  }
+}
