0ea01d5b971f9307f0d7f085755929269067459c
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-08-01 16:30:03 +0500

    Extract url parameter in ts file

--- Project 9 working with routing and SPA/src/app/app.routes.ts
+++ Project 9 working with routing and SPA/src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { TaskComponent } from './tasks/task/task.component';
+import { UserComponent } from './users/user/user.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: NoTaskComponent,
   },
+  // {
+  //   path: 'tasks/:userId',
+  //   component: TaskComponent,
+  // },
   {
-    path: 'tasks/:userId',
-    component: TaskComponent,
+    path: 'user/:userId',
+    component: UserTasksComponent,
   },
 ];
