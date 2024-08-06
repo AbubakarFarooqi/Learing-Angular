ef8810c70bc553fd41e8ba6301373b36c8d322e0
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-08-06 12:02:14 +0500

    Add route guard in user route

--- Project 9 working with routing and SPA/src/app/app.routes.ts
+++ Project 9 working with routing and SPA/src/app/app.routes.ts
@@ -1,4 +1,10 @@
-import { Routes } from '@angular/router';
+import {
+  CanMatch,
+  CanMatchFn,
+  RedirectCommand,
+  Router,
+  Routes,
+} from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { UserComponent } from './users/user/user.component';
@@ -6,6 +12,16 @@ import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { inject } from '@angular/core';
+
+const dummyCanMatchRouteGuard: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  let rnd = Math.random();
+  if (rnd < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+};
 
 export const routes: Routes = [
   {
@@ -19,6 +35,7 @@ export const routes: Routes = [
   {
     path: 'user/:userId',
     component: UserTasksComponent,
+    canMatch: [dummyCanMatchRouteGuard],
     children: [
       {
         path: '',
