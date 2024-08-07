efa61f1742101a2fdc355e83d59d10e64219cf26
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-08-07 16:56:35 +0500

    Header component search form is implemented using reactive form approach

--- Project 10 Ecom Website Mega Project(Hope So )/src/app/header/header.component.ts
+++ Project 10 Ecom Website Mega Project(Hope So )/src/app/header/header.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
+  imports: [ReactiveFormsModule],
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  searchForm = new FormGroup({
+    searchInput: new FormControl('', [Validators.required]),
+  });
+  onSearch() {
+    if (this.searchForm.invalid) {
+      return;
+    }
+    // Implement Search logic here in future
+    this.searchForm.reset();
+  }
+}
