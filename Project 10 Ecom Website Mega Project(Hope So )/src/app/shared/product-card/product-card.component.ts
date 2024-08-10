4cb40772507a036d58c48d4b5be89dbcf0fc0f28
AbubakarFarooqi <azanalifarooqi@gmail.com>
2024-08-10 14:19:04 +0500

    product card component created

--- Project 10 Ecom Website Mega Project(Hope So )/src/app/shared/product-card/product-card.component.ts
+++ Project 10 Ecom Website Mega Project(Hope So )/src/app/shared/product-card/product-card.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { StarRatingComponent } from '../star-rating/star-rating.component';
-
+import { CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [StarRatingComponent],
+  imports: [StarRatingComponent, CurrencyPipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  @Input({ required: true }) title!: string;
+  @Input({ required: true }) price!: number;
+  @Input() rating?: number;
+}
